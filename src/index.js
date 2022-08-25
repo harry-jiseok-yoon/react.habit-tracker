27e725345e66fec4cb765c4f0a71b9665712165d
macolate <jisok13@gmail.com>
2022-08-25 17:49:07 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import SimpleHabit from './components/simpleHabit';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SimpleHabit />
+    <App />
 </React.StrictMode>,
 );
